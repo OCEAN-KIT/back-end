7ae5b0c618122055ca8674f05edcec8903224c0e
jungjiyu <libraryofjiyu@gmail.com>
2025-12-02 10:15:03 +0900

    chore: 개발 편의 위해 CORS 설정 전체 허용으로 변경

--- src/main/java/com/ocean/piuda/security/jwt/config/WebConfig.java
+++ src/main/java/com/ocean/piuda/security/jwt/config/WebConfig.java
@@ -10,26 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        // 프론트 개발
-                        "http://localhost:5500",
-                        "http://127.0.0.1:5500",
-                        "http://localhost:3000",
-                        "http://localhost:5173",
-                        "http://localhost:4173",
-                        "https://localhost:3000",
-                        "https://localhost:5173",
-
-                        // 프론트 배포
-                        "https://어쩌구저쩌구.vercel.app",
-
-                        //스웨거용
-                        "https://www.jungjiyu.com",
-                        "http://www.jungjiyu.com"
-                )
+                // allowCredentials(true)일 때는 allowedOrigins("*") 대신 allowedOriginPatterns("*")를 사용해야 합니다.
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true); // 쿠키/인증 포함 시 필요
+                .allowCredentials(true);
     }
 
 }
